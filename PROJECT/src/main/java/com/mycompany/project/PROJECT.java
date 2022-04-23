32311a93ee1bf5bb3299f4a2c8248b1ccb406970
Rael Souza da Paixão <rsouza2123@gmail.com>
2022-04-23 01:01:44 -0300

    Função de config

--- PROJECT/src/main/java/com/mycompany/project/PROJECT.java
+++ PROJECT/src/main/java/com/mycompany/project/PROJECT.java
@@ -5,13 +5,49 @@
 
 package com.mycompany.project;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rael
  */
 public class PROJECT {
-
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("*INICIO*");
+        String lcResp;
+        Scanner scan = new Scanner(System.in);
+
+            Iniciar();
+            System.out.print(">");
+            lcResp = scan.nextLine();
+
+            if(lcResp.contains("/c ")){
+                lcResp = lcResp.substring(3);
+                System.out.println(lcResp);
+                Config(lcResp); 
+            }
+    }
+    
+    public static void Iniciar(){
+        
+    }
+    
+    public static void Config(String opcao){
+        switch(opcao){
+            case "Reiniciar":
+                Iniciar();
+            case "Creditos":
+                System.out.println("Jogo produzido por:\n" +
+                    "Rael Souza da Paixão\n" +
+                    "Vitor Manoel Batista de Araujo\n" +
+                    "Vinícius Matias\n" +
+                    "Vinícius Santana\n" +
+                    "Rafael\n" +
+                    "Lucas");
+            case "Sair":
+                System.exit(0);        
+        }
     }
 }
