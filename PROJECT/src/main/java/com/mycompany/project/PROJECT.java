d49131a80ce48bea28396415aa7ec8582fdb2470
RaelSouza <>
2022-05-22 19:22:19 +0000

    Arrumando códigos

--- PROJECT/src/main/java/com/mycompany/project/PROJECT.java
+++ PROJECT/src/main/java/com/mycompany/project/PROJECT.java
@@ -13,25 +13,18 @@ import java.util.Scanner;
  */
 public class PROJECT {
     public static int[] Status;
+    Scanner scan = new Scanner(System.in);
     
     public static void main(String[] args) {
       Status[0] = 50;//vida
 	    Status[1] = 10;//balas
 	    Status[2] = 2;//mana
 	    Status[3] = 0;//chave
-        System.out.println("*INICIO*");
-        String lcResp;
-        Scanner scan = new Scanner(System.in);
-
-            Iniciar();
-            System.out.print(">");
-            lcResp = scan.nextLine();
-
-            if(lcResp.contains("/c ")){
-                lcResp = lcResp.substring(3);
-                System.out.println(lcResp);
-                Config(lcResp); 
-            }
+      Status[4] = 60;//municão
+      Status[5] = 3;//PoteMana
+      Status[6] = 3;//PoteVida
+      Status[7] = 3;//Granadas
+      Status[8] = 0;
     }
       public static void dano1(int a) {
   		status[0] = status[0] - 5;
@@ -40,219 +33,166 @@ public class PROJECT {
   		status[0] = status[0] - 10;		
   	}
   	public static void atirar(int a) {
-  		status[1] = status[1] - 1;
+      if (status[1] > 0){
+        status[1] = status[1] - 1;
+      }	
   	}
   	public static void recarregar(int a) {
-  		status[1] = 10;
-  	}
-  	public static void poção(int a) {
-  		status[0]= status[0] + 20;
-  	}
-  	public static void MenosMana(int a) {
-  		status[2]= status[2] -1;
-  	}
-  	public static void PoteMana(int a) {
-  		status[2]= 2;
-  	}
-  	public static void chave(int a) {
-  		status[3]= 1;
-  	}
-  	public static void MenosChave(int a) {
-  		status[3]= status[3] - 1;
-  	}
-    
-    public static void Iniciar(){
-        
-    }
-    
-    public static void Config(String opcao){
-        switch(opcao){
-            case "Reiniciar":
-                Iniciar();
-            case "Creditos":
-                System.out.println("Jogo produzido por:\n" +
-                    "Rael Souza da Paixão\n" +
-                    "Vitor Manoel Batista de Araujo\n" +
-                    "Vinícius Matias\n" +
-                    "Vinícius Santana\n" +
-                    "Rafael\n" +
-                    "Lucas");
-            case "Sair":
-                System.exit(0);        
-        }    
-    }
-    
-    public static void Status(int[] status,String local, String escolha, boolean poder){
-        System.out.println("Vida:" + status[1] +"\n" +
-            "Resistencia:" + status[2] + "\n" +
-            "Local: " + local);
-        
-        System.out.println("Escolhas:");
-        
-        if (escolha.equals("Movimentação")){
-            System.out.println("Norte\n" +
-                "Leste\n" +
-                "Oeste\n" +
-                "Sul");
-        }else if(escolha.equals("Combate")){
-            System.out.println("Atacar\n" +
-                "Fugir\n" +
-                "Esquivar");
+      if (status[4] > 0){
+        if (status[4] >= 10){
+          status[1] = 10;
+          status[4] = status[4] - 10;
+        }else{
+          status[1] = status[4];
+          status[4] = status[4];
         }
-        
-        System.out.println("");
-        if(poder){
-            System.out.println("Seu poder pode ser usado!");
-        }
+      }else{
+        System.out.println("Não Recarregou");
+      }	
+  	}
+  public static void Granada(int a) {
+    if (status[7] > 0){
+      status[7] = status[7] - 1; 
+    }else{
+      System.out.println("Não tem mais granadas!");
     }
     
-    public static boolean Sorte(){
-        
-        return true;
+  }
+  
+  public static void PoteMana(int a) {
+    if (status[5] > 0 && status[2] = 0){
+      status[2]= 2;
+    }else{
+      System.out.println("Você pode não ter mais potes de mana, ou ainda lhe sobram cargas de poder");
     }
-    public static void PrimeiraFase(int[] status){
-        Scanner scan = new Scanner(System.in);
+  }
+  
+  public static void chave(int a) {
+    status[3]= 1;
+  }
+  
+  public static void MenosChave(int a) {
+    status[3]= 0;
+  }
+  
+  public static void Config(String opcao){
+      switch(opcao){
+          case "Reiniciar":
+          
+          case "Creditos":
+            System.out.println("Jogo produzido por:\n" +
+                "Rael Souza da Paixão\n" +
+                "Vitor Manoel Batista de Araujo\n" +
+                "Vinícius Matias\n" +
+                "Vinícius Santana\n" +
+                "Rafael");
+          case "Sair":
+            System.exit(0);
+          default:
+            System.out.println("Opções > Creditos , Reiniciar , Sair");
+      }    
+  }
+  
+  public static void MostrarStatus(String local, boolean poder){
+    System.out.println("Vida: " + status[0] +"\n" +
+                      "Poder: " + status[2] +"\n" +
+                      "Balas na arma: " + status[1] + "\n" +
+                      "Munição: "+ Status[4] +
+                      "Local: " + local +
+                      "Potes de mana: " + status[5] +"\n" +
+                      "Potes de vida: " + status[6]);
+    
+    System.out.println("Escolhas:");
+  
+    if(poder && Status[2] > 0){
+        System.out.println("Seu poder pode ser usado!");
+    }
+  }
 
-        System.out.println("Deseja jogar o tutorial?");
-        String lcTutorial = scan.next();
-        
-        if ("SIM".equals(lcTutorial.toUpperCase())){
-            System.out.println("Bem vindo a área de treino da Atlas!\n"
-                    + " aqui você vai aprender os comandos básicos e funcionalidades da sua equipe!");
-            
-            System.out.println("");
-            System.out.println("Vamos começar!");
-            System.out.println("");
-            
-            String lcRosa = "";
-            do{
-                System.out.println("Estamos na entrada, digite NORTE para entrar no campo.");
-                lcRosa = scan.next().toUpperCase();
-
-            }while(lcRosa.equals("NORTE"));
-            
-            System.out.println("Entramos no stand de tiros.\n" +
-                "");
-            
-            String lcPegar = "";
-            boolean llRifle = false;
-            do{
-                System.out.println("digite PEGAR RIFLE para pegar sua arma inicial.");
-                lcPegar = scan.next().toUpperCase();
-                
-                if(lcPegar.equals("PEGAR RIFLE")){
-                    llRifle = true;
-                }else{
-                    if(lcPegar.contains("PEGAR")){
-                        System.out.println("Não tem esse item nesta área");
-                    }
-                }
-
-            }while(!lcRosa.equals("PEGAR RIFLE"));
-            
-            System.out.println("Temos alguns alvos a frente, você pode MIRAR para ter melhor precisão, ou ATIRAR aleatóriamente.\n" +
-                "A escolha é sua agora!");
-            
-            String lcEscolha = "";
-            do{
-                lcEscolha = scan.next().toUpperCase();
-                
-                if(lcEscolha.equals("MIRAR")){
-                    System.out.println("Tiros perfeitos, parabéns!");
-                }else{
-                    boolean llTiro = Sorte();
-                    
-                    if(llTiro){
-                        System.out.println("Tiros perfeitos, parabéns!");
-                    }else{
-                        System.out.println("Você errou, mais sorte na próxima vez!");
-                    }
-                    
-                }
-                
-                System.out.println("Deseja passar para a próxima etapa agora? você pode ficar aqui mais um tempo se quiser.");
-                lcEscolha = scan.next();
-                
-                if(!lcEscolha.equals("SIM")){
-                   /*
-                        Dar nvl2 Pra mira
-                    */
-                    System.out.println("Pode MIRAR ou ATIRAR novamente.");
-                }
-            }while(lcEscolha.equals("SIM"));
-
-            do{
-                System.out.println("Vamos ver se já sabe usar um pouco das coisas que adicionaram no seu corpo.\n"
-                    + "digite LESTE para ir para a área livre");
-                lcRosa = scan.next().toUpperCase();
-
-            }while(lcRosa.equals("LESTE"));
-            
-            System.out.println("Na área livre você pode testar granadas, estilos de combate e seus poderes.");
-            
-            System.out.println("Vamos começar pelas granadas, elas são ajustadas para seguir aqueles bichos, então fique tranquilo quanto a mira;");
-            System.out.println("Os bonecos nessa área estão sujos com aquela gosma dos visitantes.");
-            
-            do{
-                System.out.println("Digite LANÇAR GRANADA e veja o que acontece");
-                lcEscolha = scan.next().toUpperCase();
-            }while(!lcEscolha.equals("LANÇAR GRANADA"));
-            
-            System.out.println("Belo lançamento!");
-            System.out.println("Mas tome cuidado, você tem uma carga máxima para essas granadas, e elas chamam bastante atenção, fique espeto!");
-            
-            System.out.println("você tem jeitos de lutar também, não precisa matar todos seus inimigos, mas sim comprir seus objetivos soldado.\n" +
-                "Vamos tentar o modo stealth para passar pelos bonecos.");
-                        
-            do{
-                System.out.println("Digite ESCONDER para ir para a proteção mais próxima.");
-                lcEscolha = scan.next().toUpperCase();
-
-            }while(!lcEscolha.equals("ESCONDER"));
-            
-            System.out.println("Perfeito, agora, precisamos avançar.\n"
-                + " a oeste você está vendo uma parede certo?\n"
-                + " e a leste temos um caixote mais próximo,\n"
-                + " faça sua escolha!");
-            
-            lcRosa = "";
-            while(!lcRosa.equals("OESTE") && !lcRosa.equals("LESTE")){
-                lcRosa = scan.next().toUpperCase();
-            }
-            
-            if (lcRosa == "Leste"){
-                System.out.println("Cuidado!\n"
-                        + " o boneco de testes te viu, ele tem poder de fogo.\n"
-                        + " Use seu PODER, para escapar, RÁPIDO!!!");
-                
-                do{
-                    System.out.println("Digite PODER e veja o que acontece");
-                    lcEscolha = scan.next().toUpperCase();
-                }while(!lcEscolha.equals("PODER"));
-                
-                switch(status[3]){
-                    case 1:
-                        System.out.println("- Você sai voando de perto do boneco de teste.");
-                    case 2:
-                        System.out.println("- Você dá um soco para frente, fazendo um efeito com o ar que acaba derrubando o boneco e o quebrando!");
-                    case 3:
-                        System.out.println("- Você usa seu poder para levantar o boneco e jogar na parede.");
-                }       
-            }
-            
-            System.out.println("Boa jogada, um caixote não me parece um lugar ideal para de esconder!");
-                
-            System.out.println("A partir daqui pode passar para NORTE, não tem mais problemas!");
-            do{
-                System.out.println("Pode passar para NORTE");
-                lcRosa = scan.next().toUpperCase();
-
-            }while(!lcRosa.equals("NORTE"));
+  public static void PrimeiraFase(int[] status){
+    Status[8] = 1;
+    String lcTutorial = Repeticao("Deseja jogar o tutorial?","SIM,NÃO");
+    
+    if ("SIM".equals(lcTutorial.toUpperCase())){
+      System.out.println("Bem vindo a área de treino da Atlas!\n"
+                        + " aqui você vai aprender os comandos básicos e funcionalidades da sua equipe!");
+      System.out.println("");
+      System.out.println("Vamos começar!");
+      System.out.println("");
+      
+      String lcRosa = Repeticao("Estamos na entrada, digite NORTE para entrar no campo.","NORTE");
+      
+      System.out.println("Entramos no stand de tiros.");
+      
+      String lcPegar = ("digite PEGAR RIFLE para pegar sua arma inicial.","PEGAR RIFLE");
+      
+      String lcEscolha = Repeticao("Temos alguns alvos a frente, você pode MIRAR para ter melhor precisão, ou ATIRAR aleatóriamente.\n"                               +"A escolha é sua agora!","MIRAR,ATIRAR");
+      
+      do{
+        if(lcEscolha.equals("MIRAR")){
+          System.out.println("Tiros perfeitos, parabéns!");
+        }else{
+          boolean llTiro = Sorteio();
+          
+          if(llTiro){
+            System.out.println("Tiros perfeitos, parabéns!");
+          }else{
+            System.out.println("Você errou, mais sorte na próxima vez!");
+          }
         }
+        System.out.println("Deseja passar para a próxima etapa agora? você pode ficar aqui mais um tempo se quiser.");
+        lcEscolha = scan.next();
         
-        System.out.println("Bem, finalizamos o treinamento.\n"
-                + "Boa sorte na sua próxima missão soldado!");
-    }
+        if(!lcEscolha.equals("SIM")){
+          System.out.println("Pode MIRAR ou ATIRAR novamente.");
+        }
+      }while(lcEscolha.equals("SIM"));
+      
+      lcRosa = Repeticao("Vamos ver se já sabe usar um pouco das coisas que adicionaram no seu corpo.\n"
+            + "digite LESTE para ir para a área livre","LESTE");
+      
+      System.out.println("Na área livre você pode testar granadas, estilos de combate e seus poderes.");
+      
+      System.out.println("Vamos começar pelas granadas, elas são ajustadas para seguir aqueles bichos, então fique tranquilo quanto a mira;");
+      System.out.println("(Os bonecos nessa área estão sujos com aquela gosma dos visitantes.)");
+      
+      lcEscolha = Repeticao("Digite LANÇAR GRANADA e veja o que acontece","LANÇAR GRANADA");
+      
+      System.out.println("Belo lançamento!");
+      System.out.println("Mas tome cuidado, você tem uma carga máxima para essas granadas, e elas chamam bastante atenção, fique espeto!");
+      
+      System.out.println("você tem jeitos de lutar também, não precisa matar todos seus inimigos, mas sim comprir seus objetivos soldado.\n"                           +"Vamos tentar o modo stealth para passar pelos bonecos.");
+                  
+      lcEscolha = Repeticao("Digite ESCONDER para ir para a proteção mais próxima.","ESCONDER");
+      
+      lcRosa = Repeticao("Perfeito, agora, precisamos avançar.\n"
+                        + " a OESTE você está vendo uma parede certo?\n"
+                        + " e a LESTE temos um caixote mais próximo,\n"
+                        + " faça sua escolha!","OESTE,LESTE");
+      
+      if (lcRosa == "LESTE"){
+        lcEscolha = Repeticao("Cuidado!\n"
+                + " o boneco de testes te viu, ele tem poder de fogo.\n"
+                + " Use seu PODER, para escapar, RÁPIDO!!","Poder");
+        
+        switch(status[3]){
+            case 1:
+                System.out.println("- Você sai voando de perto do boneco de teste.");
+            case 2:
+                System.out.println("- Você dá um soco para frente, fazendo um efeito com o ar que acaba derrubando o boneco e o quebrando!");
+            case 3:
+                System.out.println("- Você usa seu poder para levantar o boneco e jogar na parede.");
+        }       
+      }
+      System.out.println("Boa jogada, um caixote não me parece um lugar ideal para de esconder!");
+          
+      System.out.println("A partir daqui pode passar para NORTE, não tem mais problemas!");
+        
+      lcRosa = Repeticao("Pode passar para NORTE","NORTE");
+  }   
+  System.out.println("Bem, finalizamos o treinamento.\n"
+          + "Boa sorte na sua próxima missão soldado!");
+  }
 
   public static String Repeticao(String Texto, String Resp){
     String lcRosa = "";
@@ -261,7 +201,12 @@ public class PROJECT {
       System.out.println(Texto);
       lcRosa = scan.next().toUpperCase();
 
-    }while(!Resp.contains(lcRosa.toUpperCase());
+      if(lcRosa.contains("CONFIG")){
+        Config(lcRosa.substring(6));
+      }
+
+      MostrarStatus(local,false);
+    }while(!Resp.contains(lcRosa);
 
     return lcRosa;
   }
@@ -276,16 +221,16 @@ public class PROJECT {
     }
   }
   }
-	public static void fundo (int a) {
-		Scanner scan = new Scanner(System.in);
+	public static void Fundo (int a) {
+    Status[8] = 31;
 		int t = 1;
 		int balas = 10;
 		int vida = 50;
 		if (t == 1) {
-			System.out.println("General- Conseguimos informaçoes que o cientista esta preso em uma instalação\n"
-					+ ", sua missão sera resgatar e traze-lo com vida, ele é essencial para o sucesso an guerra\n"
-					+ "e sem ele nosso planeta corre serios riscos, você irá entrar pelos fundos furtivamente\n"
-					+ ", devera entrar e sair sem alertar, seu companheiro dara um jeito de chamar atençao dos guardas\n"
+			System.out.println("General- Conseguimos informaçoes que o cientista esta preso em uma instalação,\n"
+					+ "sua missão será resgatar e traze-lo com vida, ele é essencial para o sucesso nesta guerra\n"
+					+ "e sem ele nosso planeta corre sérios riscos, você irá entrar pelos fundos furtivamente,\n"
+					+ "devera entrar e sair sem alertar, seu companheiro dara um jeito de chamar atençao dos guardas\n"
 					+ "para eles sairem e facilitar sua entrada e saida, boa sorte!.");
 			System.out.println("O seu companheiro está chamando atençao para distrair os guardas, \n"
 					+ "você escolhe o: \ncaminho 1(ir pelos fundos)\ncaminho2(entrar pelo tunel do esgoto?)");
@@ -621,7 +566,7 @@ public class PROJECT {
 
 public static void Fase3Caminho1(){
     System.out.println("-Você está em um salão grande e vazio e aparentemente sem inimigos /n"+
-                       "-Na sala parece que aconteceu uma intensa luta, com uma gosma estranha(Provavel sangue dos mesmos) /n");
+                       "-Na sala parece que aconteceu uma intensa luta, com uma gosma estranha(Provável sangue dos mesmos) /n");
     String resp = Repeticao("- Tem Salas tanto para a OESTE quanto para LESTE","LESTE,OESTE");
 
     if(resp.equals("LESTE")){
@@ -667,6 +612,11 @@ public static void Fase3Caminho5(){
 
   String lcResp = Repeticao("Está preparado?","SIM,NÃO");
 
+  if (!Status[3] > 0){
+    lcResp = "NÃO";
+
+    System.out.println("Você não achou a chave desta sala, vasculhe outras salas antes de proceguir");
+  }
   if (lcResp == "SIM"){
     Status[3] = Status[3] - 1
     System.out.println("-Sua chave foi gasta!");
@@ -675,13 +625,12 @@ public static void Fase3Caminho5(){
     Wave();
 
     System.out.println("Mandou bem, acho que não preciso mais te chamar de soldado, /n quando voltarmos talvez seja coronel ou mais agora!(Risos)");
-    
-    Fase3Final();
-    
+    Fase3Chefe();
   }else{
     Fase3Caminho1();
   }
 }
+
 public static void Fase3Final(){
   
 
